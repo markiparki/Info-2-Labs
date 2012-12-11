0d83ec4c80550a45de88e956c681124de1c96a30
JW <jshwdmnn@gmail.com>
2012-12-11 13:12:47 +0100

    Fenster erstellt.

--- Ex_09_Recursive_Triangles/src/Draw.java
+++ Ex_09_Recursive_Triangles/src/Draw.java
@@ -2,11 +2,17 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Draw {
+	JFrame frame;
 	
 	public Draw() {
+		frame = new JFrame("draw");
+		frame.setSize(1366, 768);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
+		new Draw();
 	}
 
 }
