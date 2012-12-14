a207156c45d22f728f43e9791716d5898e87026f
JW <jshwdmnn@gmail.com>
2012-12-14 18:01:36 +0100

    Sierpinski Dreieck unfarbig...VERDAMMT NOCHMAL WIE GEIL

--- Ex_09_Recursive_Triangles/src/Draw.java
+++ Ex_09_Recursive_Triangles/src/Draw.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 public class Draw extends JFrame {
 	Random r = new Random();
 	JFrame frame;
-	int steps = 0;
 
 	public Draw() {
 		super();
@@ -41,26 +40,27 @@ public class Draw extends JFrame {
 	public void drawTriangle(Point p1, Point p2, Point p3, Graphics g) {
 		int[] x1 = { p1.x, p2.x, p3.x };
 		int[] y1 = { p1.y, p2.y, p3.y };
-		
-		g.setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
-		g.fillPolygon(x1, y1, x1.length);
+				
+		//g.setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+		g.drawPolygon(x1, y1, x1.length);
 	}
 	
 	public void drawTriangleRecursive(Point p1, Point p2, Point p3, Graphics g) {
-		steps++;
-		if(steps > 8) {
-			steps = 0;
+		if(Math.sqrt( (p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y) ) < 5) {
 			return;
 		}
+		
 		Point p4 = new Point ((p1.x + p2.x)/2, (p1.y + p2.y)/2);
 		Point p5 = new Point ((p2.x + p3.x)/2, (p2.y + p3.y)/2);
 		Point p6 = new Point ((p3.x + p1.x)/2, (p3.y + p1.y)/2);
-
-		int[] x2 = { p4.x, p5.x, p6.x };
-		int[] y2 = { p4.y, p5.y, p6.y };
 		
-		g.setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
-		g.fillPolygon(x2, y2, x2.length);
-		drawTriangleRecursive(p4,p5,p6, g);
+		int[] x2 = { p4.x, p5.x, p6.x };
+		int[] y2 = { p4.y, p5.y, p6.y };		
+		
+		//g.setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+		g.drawPolygon(x2, y2, x2.length);
+		drawTriangleRecursive(p1, p4, p6, g);
+		drawTriangleRecursive(p4, p2, p5, g);
+		drawTriangleRecursive(p6, p5, p3, g);
 	}
 }
