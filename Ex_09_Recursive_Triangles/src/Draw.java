82944ac70f881e631f7cbb3cc0803a22dc7c9072
JW <jshwdmnn@gmail.com>
2012-12-14 18:12:46 +0100

    Farbiges, fertiges Sierpinski Dreieck.

--- Ex_09_Recursive_Triangles/src/Draw.java
+++ Ex_09_Recursive_Triangles/src/Draw.java
@@ -32,24 +32,23 @@ public class Draw extends JFrame {
 		
 		drawTriangle(p1, p2, p3, g);
 		drawTriangle(p4, p5, p6, g);
-		drawTriangleRecursive(p1, p4, p6, g);
-		drawTriangleRecursive(p4, p5, p2, g);
-		drawTriangleRecursive(p3, p6, p5, g);
+		drawTriangleRecursive(p1, p2, p3, g);
 	}
 	
 	public void drawTriangle(Point p1, Point p2, Point p3, Graphics g) {
 		int[] x1 = { p1.x, p2.x, p3.x };
 		int[] y1 = { p1.y, p2.y, p3.y };
-				
-		//g.setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+		
 		g.drawPolygon(x1, y1, x1.length);
 	}
 	
 	public void drawTriangleRecursive(Point p1, Point p2, Point p3, Graphics g) {
+		//Wenn die Seite des Dreiecks kuerzer ist als 5 Pixel -> Abbruch
 		if(Math.sqrt( (p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y) ) < 5) {
 			return;
 		}
 		
+		//Mittelpunkte des großen Dreiecks (P1, P2, P3)
 		Point p4 = new Point ((p1.x + p2.x)/2, (p1.y + p2.y)/2);
 		Point p5 = new Point ((p2.x + p3.x)/2, (p2.y + p3.y)/2);
 		Point p6 = new Point ((p3.x + p1.x)/2, (p3.y + p1.y)/2);
@@ -57,10 +56,17 @@ public class Draw extends JFrame {
 		int[] x2 = { p4.x, p5.x, p6.x };
 		int[] y2 = { p4.y, p5.y, p6.y };		
 		
-		//g.setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
-		g.drawPolygon(x2, y2, x2.length);
+		//Zufällige Farbe für das Dreieck waehlen
+		g.setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+		//Farbiges Dreieck im gößeren Dreieck zeichnen
+		g.fillPolygon(x2, y2, x2.length);
+		//Für jedes der drei neu entstandenen Dreiecke:
+		
+		//unten links im Eck zeichnen
 		drawTriangleRecursive(p1, p4, p6, g);
+		//oben im Eck zeichnen
 		drawTriangleRecursive(p4, p2, p5, g);
+		//unten rechts im Eck zeichnen
 		drawTriangleRecursive(p6, p5, p3, g);
 	}
 }
