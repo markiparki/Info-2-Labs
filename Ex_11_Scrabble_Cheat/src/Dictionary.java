cf0ee5d904b7af0609448234a8d58558b991c6a8
JW <jshwdmnn@gmail.com>
2013-01-08 18:02:21 +0100

    kommentiert

--- Ex_11_Scrabble_Cheat/src/Dictionary.java
+++ Ex_11_Scrabble_Cheat/src/Dictionary.java
@@ -9,11 +9,13 @@ import java.util.LinkedList;
 public class Dictionary {
 	HashTable hT;
 	
+	//Initializes the HashTable and fills the Dictionary with the words from the textfile 
 	public Dictionary(String textFile) throws IOException {
-		hT = new HashTable(20);
+		hT = new HashTable(6000);
 		fillDictionary(textFile);
 	}
 	
+	//Fills dictionary's hash table with all the words seperated by a space from the textfile
 	private void fillDictionary(String textFile) throws IOException {
 		ArrayList<String> lineList = getFileLines(textFile);
 		Iterator<String> it = lineList.iterator();
@@ -23,11 +25,12 @@ public class Dictionary {
 			String[] words = line.split(" ");
 			
 			for(int i = 0; i < words.length; i++) {
-				hT.addWord(words[i].toUpperCase());
+				hT.addWord(words[i]);
 			}
 		}
 	}
 	
+	//Returns each line from the textfile as an ArrayList
 	private ArrayList<String> getFileLines(String textFile) throws IOException {
 		FileReader fr = new FileReader(textFile);
 	    BufferedReader br = new BufferedReader(fr);
@@ -43,6 +46,7 @@ public class Dictionary {
 	    return lineList;
 	}
 	
+	//Returns all the permutations from the word passed in, which are stored in the dictionary
 	public String[] lookup(String word) {
 		int index = hT.getHashValue(word);
 		LinkedList<String> wordList = hT.getWordList(index);
--- Ex_11_Scrabble_Cheat/src/HashTable.java
+++ Ex_11_Scrabble_Cheat/src/HashTable.java
@@ -3,24 +3,29 @@ import java.util.LinkedList;
 public class HashTable {
 	LinkedList<String>[] hashTable;
 	
+	//Initializes the hash table with the sized passed in
 	@SuppressWarnings("unchecked")
 	public HashTable(int size) {
 		hashTable = (LinkedList<String>[]) new LinkedList[size];
 	}
 	
+	//Adds the word passed in to the HashTable at the index according to the hash value
 	public void addWord(String word) {
+		word = word.toUpperCase();
 		hashTable[getHashValue(word)].add(word);
 	}
 	
+	//Returns all the permuations stored at the specific index
 	public LinkedList<String> getWordList(int index) {
 		return hashTable[index];
 	}
 	
+	//Returns the hash value of the word passed in
 	public int getHashValue(String word) {
 		char[] letters = word.toCharArray();
 		int hashValue = 0;
 		for(int i = 0; i < letters.length; i++) {
-			hashValue *= letters[i]*(Math.pow(31, letters.length-(i+1)));
+			hashValue *= (letters[i]-64)*(Math.pow(31, letters.length-(i+1)));
 		}
 		return hashValue;
 	}
