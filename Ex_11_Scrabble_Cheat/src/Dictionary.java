b4803eb82fe26ad538219f555734f0209f4adf3f
JW <jshwdmnn@gmail.com>
2013-01-10 01:01:57 +0100

    Formatiert.

--- Ex_11_Scrabble_Cheat/src/Dictionary.java
+++ Ex_11_Scrabble_Cheat/src/Dictionary.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-
 public class Dictionary {
 	HashTable hT;
 	
--- Ex_11_Scrabble_Cheat/src/ScrabbleCheat.java
+++ Ex_11_Scrabble_Cheat/src/ScrabbleCheat.java
@@ -1,15 +1,13 @@
-import java.io.IOException;
-
 
 public class ScrabbleCheat {
 	
-	public ScrabbleCheat() throws IOException {
+	public ScrabbleCheat() throws Exception {
 		Dictionary dic = new Dictionary("words.txt", 20000);
 		
 		System.out.println(dic.lookup("Papierkorb"));
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		new ScrabbleCheat();
 	}
 }
